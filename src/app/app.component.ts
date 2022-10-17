a7b319547a9274ec5f46bed42327c7d1c977ac5f
Tobias Fernkorn <fernsikos@gmail.com>
2022-10-17 10:16:13 +0700

    added images for slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  slideshowArray = ['code.jpg', 'macbook.jpg', 'programming.jpg', 'programming2.jpg','website.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateSlideshow()
+  };
 
 
+  updateSlideshow() {
 
-  
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.slideshowArray.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1);
+    }, 5000);
+  };
+
+
 }
