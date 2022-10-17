0e1f5c4037741a59aa9d85f77169c010d455b76d
Tobias Fernkorn <fernsikos@gmail.com>
2022-10-17 11:17:19 +0700

    added headlines and animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  slideshowArray = ['code.jpg', 'macbook.jpg', 'programming.jpg', 'programming2.jpg','website.jpg'];
+  slideshowArray = ['code.jpg', 'macbook.jpg', 'programming2.jpg','website.jpg'];
+  headlineArray = ['Bring engineering to the next level', 'Custom tailored solutions', 'Close relationship with customers', 'Business-Website integration']
   currentImage = 0;
   showImage = true;
 
